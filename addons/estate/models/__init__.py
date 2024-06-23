100ae696416c1a4052ec7e1117ea144d74d15ae1
tanvir52230 <tanvirbintouhid5230@gmail.com>
2024-06-23 22:27:48 +0600

    estate addons created

---
+++ addons/estate/models/__init__.py
@@ -0,0 +1,1 @@
+from estate_property import TestModel
---
+++ addons/estate/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import models
+
+
+class TestModel(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
+
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Availability Date")
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Number of Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
